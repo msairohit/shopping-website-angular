e159a49ee1ea7afeb7ac168cddf4d4eddc87f663
sai rohit <msrohit1919@gmail.com>
2020-05-31 17:36:22 +0530

    sign up form with basic fields completed with validations.

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidator } from '../custom-validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,30 +12,55 @@ export class SignUpComponent implements OnInit {
 
   signUpForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.signUpForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.minLength(3)]],
-      userName: ['', Validators.required],
-      address: this.formBuilder.group({
-        city: [''],
-        state: [''],
-        zip: ['']
-      }),
-      email: [''],
-      password: []
+      userName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*'), Validators.minLength(5)]],
+      password: ['', Validators.compose([
+        Validators.required,
+        // check whether the entered password has a number
+        CustomValidator.patternValidator(/\d/, {
+          hasNumber: true
+        }),
+        // check whether the entered password has upper case letter
+        CustomValidator.patternValidator(/[A-Z]/, {
+          hasCapitalCase: true
+        }),
+        // check whether the entered password has a lower case letter
+        CustomValidator.patternValidator(/[a-z]/, {
+          hasSmallCase: true
+        }),
+        // check whether the entered password has a special character
+        CustomValidator.patternValidator(
+          /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+          {
+            hasSpecialCharacters: true
+          }
+        ),
+        Validators.minLength(8)
+      ])],
+      confirmPassword: ['', Validators.compose([Validators.required])],
+      // city: [''],
+      // state: [''],
+      // zip: [''],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.max(9999999999), Validators.min(10), Validators.pattern('[0-9]{10}')]]
+    }, {
+      // check whether our password and confirm password match
+      validator: CustomValidator.passwordMatchValidator
     });
   }
 
-  isFirstNameValid() {
-    return this.signUpForm.get('firstName').invalid && (this.signUpForm.get('firstName').dirty || this.signUpForm.get('firstName').touched);
-    // return this.signUpForm.get('firstName').touched && this.signUpForm.get('firstName').invalid;
+  isValid(formControlName) {
+    return this.signUpForm.get(formControlName).invalid && (this.signUpForm.get(formControlName).dirty || this.signUpForm.get(formControlName).touched);
   }
-  isLastNameValid() {
-    return this.signUpForm.get('lastName').invalid && (this.signUpForm.get('lastName').dirty || this.signUpForm.get('lastName').touched);
-    // return this.signUpForm.get('firstName').touched && this.signUpForm.get('firstName').invalid;
+
+  onSubmit(value) {
+    console.log(value);
+    this.router.navigate(['test']);
   }
 
 }

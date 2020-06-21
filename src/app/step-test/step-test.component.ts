8105a33fa9b2f77568d2b20edbfbcaea4dbe1423
sai rohit <msrohit1919@gmail.com>
2020-06-21 16:04:43 +0530

    added licenses for the two templates used and fixed jqueryUI dependency error

--- src/app/step-test/step-test.component.ts
+++ src/app/step-test/step-test.component.ts
@@ -57,89 +57,110 @@ export class StepTestComponent implements OnInit {
   }
 
   ngAfterViewInit() {
+    // jQuery time
+    var current_fs, next_fs, previous_fs; // fieldsets
+    var left, opacity, scale; // fieldset properties which we will animate
+    var animating; // flag to prevent quick multi-click glitches
 
-    //jQuery time
-    var current_fs, next_fs, previous_fs; //fieldsets
-    var left, opacity, scale; //fieldset properties which we will animate
-    var animating; //flag to prevent quick multi-click glitches
+    jQuery(".next").click(
+      function () {
+        if (animating) {
+          return false;
+        }
+        animating = true;
 
+        current_fs = jQuery(this).parent();
+        next_fs = jQuery(this).parent().next();
 
-    $(".next").click(function () {
-      if (animating) return false;
-      animating = true;
+        // activate next step on progressbar using the index of next_fs
+        jQuery("#progressbar li").eq(jQuery("fieldset").index(next_fs)).addClass("active");
 
-      current_fs = $(this).parent();
-      next_fs = $(this).parent().next();
+        // show the next fieldset
+        next_fs.show();
+        // hide the current fieldset with style
+        current_fs.animate(
+          { opacity: 0 },
+          {
+            step: function (now, mx) {
+              // as the opacity of current_fs reduces to 0 - stored in "now"
+              // 1. scale current_fs down to 80%
+              scale = 1 - (1 - now) * 0.2;
+              // 2. bring next_fs from the right(50%)
+              left = (now * 50) + "%";
+              // 3. increase opacity of next_fs to 1 as it moves in
+              opacity = 1 - now;
+              current_fs.css(
+                {
+                  'transform': 'scale(' + scale + ')',
+                  'position': 'absolute'
+                }
+              );
+              next_fs.css({ 'left': left, 'opacity': opacity });
+            },
+            duration: 800,
+            complete: function () {
+              current_fs.hide();
+              current_fs.css({ 'position': 'relative' });
+              next_fs.css({ 'position': 'relative' });
+              animating = false;
+            },
+            // this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+          }
+        );
+      }
+    );
 
-      //activate next step on progressbar using the index of next_fs
-      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    jQuery(".previous").click(
+      function () {
+        if (animating) {
+          return false;
+        }
+        animating = true;
 
-      //show the next fieldset
-      next_fs.show();
-      //hide the current fieldset with style
-      current_fs.animate({ opacity: 0 }, {
-        step: function (now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale current_fs down to 80%
-          scale = 1 - (1 - now) * 0.2;
-          //2. bring next_fs from the right(50%)
-          left = (now * 50) + "%";
-          //3. increase opacity of next_fs to 1 as it moves in
-          opacity = 1 - now;
-          current_fs.css({
-            'transform': 'scale(' + scale + ')',
-            'position': 'absolute'
-          });
-          next_fs.css({ 'left': left, 'opacity': opacity });
-        },
-        duration: 800,
-        complete: function () {
-          current_fs.hide();
-          animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
-      });
-    });
+        current_fs = jQuery(this).parent();
+        previous_fs = jQuery(this).parent().prev();
 
-    $(".previous").click(function () {
-      if (animating) return false;
-      animating = true;
+        // de-activate current step on progressbar
+        jQuery("#progressbar li").eq(jQuery("fieldset").index(current_fs)).removeClass("active");
 
-      current_fs = $(this).parent();
-      previous_fs = $(this).parent().prev();
+        // show the previous fieldset
+        previous_fs.show();
+        previous_fs.css({ 'position': 'absolute' });
 
-      //de-activate current step on progressbar
-      $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+        // hide the current fieldset with style
+        current_fs.animate(
+          { opacity: 0 },
+          {
+            step: function (now, mx) {
+              // as the opacity of current_fs reduces to 0 - stored in "now"
+              // 1. scale previous_fs from 80% to 100%
+              scale = 0.8 + (1 - now) * 0.2;
+              // 2. take current_fs to the right(50%) - from 0%
+              left = ((1 - now) * 50) + "%";
+              // 3. increase opacity of previous_fs to 1 as it moves in
+              opacity = 1 - now;
+              current_fs.css({ 'left': left });
+              previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+            },
+            duration: 800,
+            complete: function () {
+              current_fs.hide();
+              previous_fs.css({ 'position': 'relative' });
+              animating = false;
+            },
+            // this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+          }
+        );
+      }
+    );
 
-      //show the previous fieldset
-      previous_fs.show();
-      //hide the current fieldset with style
-      current_fs.animate({ opacity: 0 }, {
-        step: function (now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale previous_fs from 80% to 100%
-          scale = 0.8 + (1 - now) * 0.2;
-          //2. take current_fs to the right(50%) - from 0%
-          left = ((1 - now) * 50) + "%";
-          //3. increase opacity of previous_fs to 1 as it moves in
-          opacity = 1 - now;
-          current_fs.css({ 'left': left });
-          previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
-        },
-        duration: 800,
-        complete: function () {
-          current_fs.hide();
-          animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: 'easeInOutBack'
-      });
-    });
-
-    $(".submit").click(function () {
-      return false;
-    })
+    jQuery(".submit").click(
+      function () {
+        return false;
+      }
+    );
 
   }
 
@@ -159,3 +180,16 @@ export class StepTestComponent implements OnInit {
 
 
 }
+
+
+/*
+Copyright (c) 2020 by David (https://codepen.io/bluestarstudios/pen/ZEGamYP)
+Fork of an original work Multi Step Form with Progress Bar using jQuery and CSS3 (https://codepen.io/atakan/pen/gqbIz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
